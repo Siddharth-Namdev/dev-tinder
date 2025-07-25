dfd033e61020a54ef94ec186390c42f0a809e598
Siddharth-Nmadev <namsiddharth19@gmail.com>
2025-07-25 22:34:29 +0530

    change DB password

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDB = async ()=>{
-    await mongoose.connect('mongodb://localhost:27017/devtinder');
-}
+const connectDB = async () => {
+  //await mongoose.connect('mongodb://localhost:27017/devtinder');
+  await mongoose.connect(
+    "mongodb+srv://namsiddharth19:Hc68qTnO6JIJ4DSZ@cluster0.phsww.mongodb.net/"
+  );
+};
 
 module.exports = connectDB;
