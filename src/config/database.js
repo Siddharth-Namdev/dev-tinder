e220e72d682b037368830a0288050c98522fe812
Siddharth-Nmadev <namsiddharth19@gmail.com>
2025-07-26 11:30:33 +0530

    update DB

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   //await mongoose.connect('mongodb://localhost:27017/devtinder');
   await mongoose.connect(
-    "mongodb+srv://namsiddharth19:Hc68qTnO6JIJ4DSZ@cluster0.phsww.mongodb.net/"
+    "mongodb+srv://namsiddharth19:Hc68qTnO6JIJ4DSZ@cluster0.phsww.mongodb.net/Dev-Tinder"
   );
 };
 
